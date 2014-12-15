8422bbb8d51578d2cc3398c7a6ae0e3ca8f590d0
Daniel Bernal <bernaldani@gmail.com>
2014-12-15 12:17:10 -0300

    Initial Commit

---
+++ FbloginiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FbloginiOS
+//
+//  Created by Daniel Bernal on 12/15/14.
+//  Copyright (c) 2014 Daniel Bernal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
